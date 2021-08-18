0b8abb60ed1a0329b484c45be2e2e63926a9a02b
Chelly izraelov <hlly.lov@gmail.com>
2021-08-18 13:14:35 +0300

    ...

--- CouponSystem_SpringProject_SessionTechnique/src/main/java/com/couponsystem/CouponSystemSpringProjectApplication.java
+++ CouponSystem_SpringProject_SessionTechnique/src/main/java/com/couponsystem/CouponSystemSpringProjectApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//TODO -> @EnableScheduling
 public class CouponSystemSpringProjectApplication {
 
 	public static void main(String[] args) {

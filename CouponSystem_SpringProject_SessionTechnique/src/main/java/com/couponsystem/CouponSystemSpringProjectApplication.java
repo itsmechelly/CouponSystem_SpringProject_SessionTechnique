20793a1604cd84112431d37bb7eaee40f4ca382c
Chelly izraelov <hlly.lov@gmail.com>
2023-02-28 11:09:20 +0200

    relo

--- CouponSystem_SpringProject_SessionTechnique/src/main/java/com/couponsystem/CouponSystemSpringProjectApplication.java
+++ CouponSystem_SpringProject_SessionTechnique/src/main/java/com/couponsystem/CouponSystemSpringProjectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CouponSystemSpringProjectApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(CouponSystemSpringProjectApplication.class, args);
 	}
 }
